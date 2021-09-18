82032ac011e66441003bb347b416b4fc1cd20c1f
F6173079 Leandro dos Santos Coutinho <lescoutinhovr@gmail.com>
2021-09-18 18:47:14 -0300

    Armeria hello world!

---
+++ run.cmd
@@ -0,0 +1,1 @@
+mvn exec:java -Dexec.mainClass=test.armeria.Main
---
+++ src/main/java/test/armeria/Main.java
@@ -0,0 +1,18 @@
+package test.armeria;
+
+import com.linecorp.armeria.common.*;
+import com.linecorp.armeria.server.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Server server = Server.builder()
+			.http(8080)
+			.https(8443)
+			.tlsSelfSigned()
+			.service("/", (ctx, req) -> HttpResponse.of("Hello Armeria!"))
+			.build();
+		
+		server.start();
+	}
+}
